fbfd4b200c6c6d819e64b192d18b5d31be0cc77b
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-12-13 04:00:47 +0300

    js

--- contacts.js
+++ contacts.js
@@ -1,8 +1,8 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
-
-const contactsPath = path.join("./db", 'contacts.json')
+const contactsPath = path.join(__dirname, './db/contacts.json')
 
 const listContacts = async () => {
     const result = await fs.readFile(contactsPath);
@@ -13,7 +13,7 @@ const getContactById = async (contactId) => {
     console.log(contactId);
     const contacts = await listContacts();
     const result = contacts.find(item => item.id === contactId);
-    return result || null
+    return result || null;
 }
 
 
@@ -21,11 +21,11 @@ const removeContact = async (contactId) => {
     const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === contactId)
     if (index === -1) {
-        return null
+        return null;
     }
     const [result] = contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-    return result
+    return result;
 }
 
 const addContact = async (name, email, phone) => {
@@ -38,7 +38,7 @@ const addContact = async (name, email, phone) => {
     }
     const contactList = JSON.stringify([...contacts, newContact], null, 2)
     await fs.writeFile(contactsPath, contactList)
-    return newContact
+    return newContact;
 }
 
 module.exports = {
